d4cc174ecf402998869720813c014e563e20eda7
Maheshbabu Bodicherla <116437937+MaheshBabu87@users.noreply.github.com>
2022-10-30 16:10:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale,Spinach & Rocket Smoothi')
-streamlit.text('Hard Boiled Free Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇
+
 
 
