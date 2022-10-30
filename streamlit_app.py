583e9f186713094bbf0afb2d345b96e6160bc6ed
Maheshbabu Bodicherla <116437937+MaheshBabu87@users.noreply.github.com>
2022-10-30 16:39:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.header('🥑🍞 Avacodo Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
