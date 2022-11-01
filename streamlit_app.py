5ef497b95b51d50c9f7ab9e5109d1731951b1318
Maheshbabu Bodicherla <116437937+MaheshBabu87@users.noreply.github.com>
2022-11-01 19:21:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
   
 streamlit.header("The fruit Load list contains:")
 def get_fruityvice_data(this_fruit_choice):
- With my_cnx.cursor() as mu_cur:
+ with my_cnx.cursor() as mu_cur:
  my_cur.execute("select * from fruit_load_list")
  return my_cur.fetchall()
 
